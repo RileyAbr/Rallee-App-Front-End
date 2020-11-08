ec32fd2f4652ec9444de3775882a010f5183dbfa
margueritemb3 <bruggermarguerite@gmail.com>
2020-11-08 02:04:12 -0600

    signup page added

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 import Profile from "./components/Profile";
 import Home from "./components/Home";
 
 const routes = [
     { path: "/login", component: Login },
+    { path: "/signup", component: SignUp },
     { path: "/profile", component: Profile },
     { path: "/", component: Home, exact: true }
 ];
